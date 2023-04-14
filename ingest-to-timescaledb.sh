d2fe6c706f02e8f6bcbdd90aef02762a776a85f8
Martin Man <mman@martinman.net>
2023-04-14 17:14:19 +0200

    ingest to timescale with message code

--- ingest-to-timescaledb.sh
+++ ingest-to-timescaledb.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 docker run -e VENUS_HOST=${VENUS_HOST:-"127.0.0.1"} \
-  -v $PWD/telegraf/telegraf-timescaledb.conf:/etc/telegraf/telegraf.conf:ro telegraf
+  -v $PWD/telegraf/telegraf-timescaledb.conf:/etc/telegraf/telegraf.conf:ro \
+  -v $PWD/telegraf/venus-mqtt-topic-to-code-lut.json:/etc/telegraf/venus-mqtt-topic-to-code-lut.json:ro \
+  telegraf:latest
